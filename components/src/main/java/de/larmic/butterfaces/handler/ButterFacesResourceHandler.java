c92c4108dcfc4d086bcbf1c193f9821dcec14a57
Lars Michaelis <mail@larmic.de>
2014-08-28 13:05:26 +0200

    BUT-18 Add ButterFacesResourceHandler

--- components/src/main/java/de/larmic/butterfaces/handler/ButterFacesResourceHandler.java
+++ components/src/main/java/de/larmic/butterfaces/handler/ButterFacesResourceHandler.java
@@ -4,9 +4,22 @@ import javax.faces.application.Resource;
 import javax.faces.application.ResourceHandler;
 import javax.faces.application.ResourceHandlerWrapper;
 
+/**
+ * Adds a timestamp parameter to each (javascript and css) resource if resource handler is enabled. Resource browser
+ * cache will be ignored on this resources until server will be redeployed. In case of this manual browser cache delete
+ * is no longer needed.
+ * <p/>
+ * Usage:
+ * Add following code to your faces-config.xml
+ * <code>
+ * <resource-handler>de.larmic.butterfaces.handler.ButterFacesResourceHandler</resource-handler>
+ * </code>
+ */
 public class ButterFacesResourceHandler extends ResourceHandlerWrapper {
 
-    private static final String CONFIGURABLE_LIBRARY_NAME = "butterfaces-configurable";
+
+    private static final String LIBRARY_CSS_NAME = "css";
+    private static final String LIBRARY_JS_NAME = "js";
 
     private ResourceHandler wrapped;
 
@@ -24,7 +37,7 @@ public class ButterFacesResourceHandler extends ResourceHandlerWrapper {
         final Resource resource = super.createResource(resourceName, libraryName);
 
         // here a check of library name could be necessary, etc.
-        if (CONFIGURABLE_LIBRARY_NAME.equals(libraryName)) {
+        if (LIBRARY_CSS_NAME.equals(libraryName) || LIBRARY_JS_NAME.equals(libraryName)) {
             return new ButterFacesResource(resource);
         }
 
