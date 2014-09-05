1c6a92e13cea27fa6b1acb768ae3a60019aa8232
Lars Michaelis <mail@larmic.de>
2014-09-05 09:40:22 +0200

    BUT-45 Create l:prettify component (in progress)

---
+++ components/src/main/java/de/larmic/butterfaces/component/html/HtmlPrettyPrint.java
@@ -0,0 +1,66 @@
+package de.larmic.butterfaces.component.html;
+
+import javax.el.ValueExpression;
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponentBase;
+
+@ResourceDependencies({
+        @ResourceDependency(library = "butterfaces-configurable", name = "prettify.css", target = "head"),
+        @ResourceDependency(library = "butterfaces-configurable", name = "prettify.js", target = "head")})
+@FacesComponent(HtmlPrettyPrint.COMPONENT_TYPE)
+public class HtmlPrettyPrint extends UIComponentBase {
+
+    public static final String COMPONENT_TYPE = "de.larmic.butterfaces.component.prettyprint";
+    public static final String COMPONENT_FAMILY = "de.larmic.butterfaces.component.family";
+    public static final String RENDERER_TYPE = "de.larmic.butterfaces.renderkit.html_basic.PrettyPrintRenderer";
+
+    protected static final String PROPERTY_LANGUAGE = "language";
+    protected static final String PROPERTY_STYLE_CLASS = "styleClass";
+    protected static final String PROPERTY_STYLE = "style";
+
+    public HtmlPrettyPrint() {
+        super();
+        this.setRendererType(RENDERER_TYPE);
+    }
+
+    @Override
+    public String getFamily() {
+        return COMPONENT_FAMILY;
+    }
+
+    public String getLanguage() {
+        return (String) this.getStateHelper().eval(PROPERTY_LANGUAGE);
+    }
+
+    public void setLanguage(final String language) {
+        this.updateStateHelper(PROPERTY_LANGUAGE, language);
+    }
+
+    public String getStyleClass() {
+        return (String) this.getStateHelper().eval(PROPERTY_STYLE_CLASS);
+    }
+
+    public void setStyleClass(final String styleClass) {
+        this.updateStateHelper(PROPERTY_STYLE_CLASS, styleClass);
+    }
+
+    public String getStyle() {
+        return (String) this.getStateHelper().eval(PROPERTY_STYLE);
+    }
+
+    public void setStyle(final String style) {
+        this.updateStateHelper(PROPERTY_STYLE, style);
+    }
+
+    private void updateStateHelper(final String propertyName, final Object value) {
+        this.getStateHelper().put(propertyName, value);
+
+        final ValueExpression ve = this.getValueExpression(propertyName);
+
+        if (ve != null) {
+            ve.setValue(this.getFacesContext().getELContext(), value);
+        }
+    }
+}
---
+++ components/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/PrettyPrintRenderer.java
@@ -0,0 +1,64 @@
+package de.larmic.butterfaces.component.renderkit.html_basic;
+
+import de.larmic.butterfaces.component.html.HtmlPrettyPrint;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.render.FacesRenderer;
+import java.io.IOException;
+
+/**
+ * Created by larmic on 31.07.14.
+ */
+@FacesRenderer(componentFamily = HtmlPrettyPrint.COMPONENT_FAMILY, rendererType = HtmlPrettyPrint.RENDERER_TYPE)
+public class PrettyPrintRenderer extends com.sun.faces.renderkit.html_basic.HtmlBasicRenderer {
+
+    public static final String ELEMENT_DIV = "div";
+    public static final String ELEMENT_PRE = "pre";
+    public static final String ATTRIBUTE_STYLE = "style";
+    public static final String ATTRIBUTE_CLASS = "class";
+
+    @Override
+    public void encodeBegin(final FacesContext context, final UIComponent component) throws IOException {
+        final ResponseWriter writer = context.getResponseWriter();
+        final HtmlPrettyPrint prettyPrint = (HtmlPrettyPrint) component;
+
+        if (prettyPrint.isRendered()) {
+            final String style = prettyPrint.getStyle();
+            final String styleClass = prettyPrint.getStyleClass();
+
+            writer.startElement(ELEMENT_DIV, component);
+
+            if (null != style) {
+                writer.writeAttribute(ATTRIBUTE_STYLE, style, "style");
+            }
+            if (null != styleClass) {
+                writer.writeAttribute(ATTRIBUTE_CLASS, "butter-component-prettyprint " + styleClass, "class");
+            } else {
+                writer.writeAttribute(ATTRIBUTE_CLASS, "butter-component-prettyprint", "class");
+            }
+
+            writer.startElement(ELEMENT_PRE, component);
+            writer.writeAttribute(ATTRIBUTE_CLASS, "prettyprint " + prettyPrint.getLanguage(), "class");
+        }
+    }
+
+    @Override
+    public void encodeEnd(final FacesContext context, final UIComponent component) throws IOException {
+        final ResponseWriter writer = context.getResponseWriter();
+        final HtmlPrettyPrint prettyPrint = (HtmlPrettyPrint) component;
+
+        if (prettyPrint.isRendered()) {
+            writer.endElement(ELEMENT_PRE);
+            writer.endElement(ELEMENT_DIV);
+
+            // call pretty print javascript on render
+            writer.startElement("script", component);
+            writer.writeAttribute("type", "text/javascript", "type");
+            writer.writeText("jQuery(function () { prettyPrint(); });", null);
+            writer.endElement("script");
+        }
+    }
+
+}
