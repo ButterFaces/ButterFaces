c424baffa22b6754b40982fbc778e04a132df230
Lars Michaelis <mail@larmic.de>
2014-09-11 07:42:51 +0200

    create new table

--- components/src/main/java/de/larmic/butterfaces/component/html/table/HtmlTable.java
+++ components/src/main/java/de/larmic/butterfaces/component/html/table/HtmlTable.java
@@ -2,13 +2,13 @@ package de.larmic.butterfaces.component.html.table;
 
 import javax.el.ValueExpression;
 import javax.faces.component.FacesComponent;
-import javax.faces.component.UIComponentBase;
+import javax.faces.component.UIData;
 
 /**
  * Created by larmic on 10.09.14.
  */
 @FacesComponent(HtmlTable.COMPONENT_TYPE)
-public class HtmlTable extends UIComponentBase {
+public class HtmlTable extends UIData {
 
     public static final String COMPONENT_TYPE = "de.larmic.butterfaces.component.table";
     public static final String COMPONENT_FAMILY = "de.larmic.butterfaces.component.family";
