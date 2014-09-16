c886f33b2ce9e7e66f0d5b08009c91f2cccac220
Lars Michaelis <mail@larmic.de>
2014-09-16 12:10:11 +0200

    BUT-66 Create new component

---
+++ components/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/GlyphiconCommandLinkRenderer.java
@@ -0,0 +1,35 @@
+package de.larmic.butterfaces.component.renderkit.html_basic;
+
+import com.sun.faces.renderkit.html_basic.CommandLinkRenderer;
+import de.larmic.butterfaces.component.html.HtmlGlyphiconCommandLink;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.ResponseWriter;
+import javax.faces.render.FacesRenderer;
+import java.io.IOException;
+
+/**
+ * Created by larmic on 16.09.14.
+ */
+@FacesRenderer(componentFamily = HtmlGlyphiconCommandLink.COMPONENT_FAMILY, rendererType = HtmlGlyphiconCommandLink.RENDERER_TYPE)
+public class GlyphiconCommandLinkRenderer extends CommandLinkRenderer {
+
+    @Override
+    protected void writeValue(final UIComponent component, final ResponseWriter writer) throws IOException {
+        writeGlyphiconIfNecessary(component, writer);
+
+        super.writeValue(component, writer);
+    }
+
+    protected void writeGlyphiconIfNecessary(final UIComponent component,
+                                             final ResponseWriter writer) throws IOException {
+        final HtmlGlyphiconCommandLink commandLink = (HtmlGlyphiconCommandLink) component;
+        final String glyphicon = commandLink.getGlyphicon();
+
+        if (glyphicon != null && !"".equals(glyphicon)) {
+            writer.startElement("span", component);
+            writer.writeAttribute("class", glyphicon, null);
+            writer.endElement("span");
+        }
+    }
+}
