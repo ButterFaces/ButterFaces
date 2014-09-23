b01bcac381feb68a5bcfb9004d59cd40eff646e8
Lars Michaelis <mail@larmic.de>
2014-09-23 18:21:18 +0200

    BUT-81 add number ajax and touchpin test

--- test/src/test/java/de/larmic/butterfaces/test/component/NumberComponentTest.java
+++ test/src/test/java/de/larmic/butterfaces/test/component/NumberComponentTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import static org.jboss.arquillian.graphene.Graphene.guardAjax;
 import static org.jboss.arquillian.graphene.Graphene.guardHttp;
@@ -22,6 +23,10 @@ public class NumberComponentTest extends AbstractComponentTest {
 
     public static final String ARQUILLIAN_CONTAINER = "arquillian_component";
     public static final String OPTION_RENDERED = "arquillian_rendered";
+    public static final String OPTION_AJAX = "arquillian_ajax";
+    public static final String OUTPUT_VALUE = "arquillian_output_value";
+    public static final String BTN_UP = "bootstrap-touchspin-up";
+    public static final String BTN_DOWN = "bootstrap-touchspin-down";
 
     @Test
     public void testNavigation() throws Exception {
@@ -46,8 +51,11 @@ public class NumberComponentTest extends AbstractComponentTest {
         Assert.assertEquals("Wrong tag name", "div", component.getTagName());
         Assert.assertEquals("Wrong tag name", "button", component.findElement(By.className("bootstrap-touchspin-up")).getTagName());
         Assert.assertEquals("Wrong tag name", "button", component.findElement(By.className("bootstrap-touchspin-down")).getTagName());
+        Assert.assertEquals("Wrong tag name", "input", this.findButterInputComponent().getTagName());
 
         this.findWebElementByClassName(OPTION_RENDERED);
+        this.findWebElementByClassName(OPTION_AJAX);
+        this.findWebElementByClassName(OUTPUT_VALUE);
     }
 
     @Test
@@ -66,6 +74,36 @@ public class NumberComponentTest extends AbstractComponentTest {
         Assert.assertNotNull("Element should be rendered but was not.", findNullableButterComponent());
     }
 
+    @Test
+    @InSequence(2)
+    public void testAjaxOption() throws Exception {
+        browser.get(deploymentUrl + COMPONENT_PAGE);
+
+        final Select showcaseAjaxOption = new Select(this.findWebElementByClassName(OPTION_AJAX));
+
+        // test no ajax
+        WebElement number = this.findButterInputComponent();
+        number.sendKeys("23");
+        number.submit();
+
+        Assert.assertEquals("23", this.findWebElementByClassName(OUTPUT_VALUE).getText());
+
+        // activate ajax
+        guardAjax(showcaseAjaxOption).selectByIndex(1);
+
+        // click button up
+        guardAjax(this.findButtonUp()).click();
+        Assert.assertEquals("24", this.findWebElementByClassName(OUTPUT_VALUE).getText());
+
+        // click button up second time
+        guardAjax(this.findButtonUp()).click();
+        Assert.assertEquals("25", this.findWebElementByClassName(OUTPUT_VALUE).getText());
+
+        // click button down
+        guardAjax(this.findButtonDown()).click();
+        Assert.assertEquals("24", this.findWebElementByClassName(OUTPUT_VALUE).getText());
+    }
+
     private WebElement findNullableButterComponent() {
         try {
             final WebElement outerComponent = this.findWebElementByClassName(ARQUILLIAN_CONTAINER);
@@ -74,6 +112,20 @@ public class NumberComponentTest extends AbstractComponentTest {
         }
 
         return null;
+    }
 
+    private WebElement findButterInputComponent() {
+        final WebElement outerComponent = this.findWebElementByClassName(ARQUILLIAN_CONTAINER);
+        return outerComponent.findElement(By.className("butter-number-component"));
+    }
+
+    private WebElement findButtonUp() {
+        final WebElement outerComponent = this.findWebElementByClassName(ARQUILLIAN_CONTAINER);
+        return outerComponent.findElement(By.className(BTN_UP));
+    }
+
+    private WebElement findButtonDown() {
+        final WebElement outerComponent = this.findWebElementByClassName(ARQUILLIAN_CONTAINER);
+        return outerComponent.findElement(By.className(BTN_DOWN));
     }
 }
