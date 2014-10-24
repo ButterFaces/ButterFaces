3fb540a22368bdece8ac86b9ac221e64b0faf652
Lars Michaelis <mail@larmic.de>
2014-10-24 16:34:13 +0200

    BUT-96 Add first version of tree showcase

---
+++ showcase/src/main/java/de/larmic/butterfaces/component/showcase/TreeShowcaseComponent.java
@@ -0,0 +1,42 @@
+package de.larmic.butterfaces.component.showcase;
+
+import de.larmic.butterfaces.model.tree.DefaultNodeImpl;
+import de.larmic.butterfaces.model.tree.Node;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+/**
+ * Created by larmic on 11.09.14.
+ */
+@Named
+@SessionScoped
+public class TreeShowcaseComponent extends AbstractShowcaseComponent implements Serializable {
+
+    public Node getTree() {
+        final Node secondFirstFirstChild = new DefaultNodeImpl("secondFirstFirstChild");
+
+        final Node secondFirstChild = new DefaultNodeImpl("secondFirstChild");
+        secondFirstChild.getSubNodes().add(secondFirstFirstChild);
+        final Node secondSecondChild = new DefaultNodeImpl("secondSecondChild");
+
+        final Node firstChild = new DefaultNodeImpl("firstChild");
+        final Node secondChild = new DefaultNodeImpl("secondChild");
+        secondChild.getSubNodes().add(secondFirstChild);
+        secondChild.getSubNodes().add(secondSecondChild);
+        final Node thirdChild = new DefaultNodeImpl("thirdChild");
+
+        final Node rootNode = new DefaultNodeImpl("rootNode");
+        rootNode.getSubNodes().add(firstChild);
+        rootNode.getSubNodes().add(secondChild);
+        rootNode.getSubNodes().add(thirdChild);
+
+        return rootNode;
+    }
+
+    @Override
+    public String getXHtml() {
+        return null;
+    }
+}
