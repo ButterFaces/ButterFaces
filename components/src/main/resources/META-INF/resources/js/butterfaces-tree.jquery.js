602ec7a7196be468938165759e8cc6c67de6b4bb
Lars Michaelis <mail@larmic.de>
2014-10-31 19:45:43 +0100

    BUT-94 disable js console log

--- components/src/main/resources/META-INF/resources/js/butterfaces-tree.jquery.js
+++ components/src/main/resources/META-INF/resources/js/butterfaces-tree.jquery.js
@@ -18,19 +18,19 @@
                 return;
             }
 
-            console.log("Collapsing class: "+ data.collapsingClass);
-            console.log("Expansion class: "+ data.expansionClass);
+            // console.log("Collapsing class: "+ data.collapsingClass);
+            // console.log("Expansion class: "+ data.expansionClass);
 
             $originalElement.find('li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
             $originalElement.find('li.parent_li > span.butter-component-tree-jquery-marker').on('click', function (e) {
                 var children = $(this).parent('li.parent_li').find(' > ul > li');
                 if (children.is(":visible")) {
                     children.hide('fast');
-                    console.log("Collapsing branch");
+                    // console.log("Collapsing branch");
                     $(this).attr('title', 'Expand this branch').removeClass(data.collapsingClass).addClass(data.expansionClass);
                 } else {
                     children.show('fast');
-                    console.log("Expanding branch");
+                    // console.log("Expanding branch");
                     $(this).attr('title', 'Collapse this branch').removeClass(data.expansionClass).addClass(data.collapsingClass);
                 }
                 e.stopPropagation();
