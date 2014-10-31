849a0adca23619224942386c0715f39ba2d58574
Lars Michaelis <mail@larmic.de>
2014-10-31 19:44:45 +0100

    fixed handlePrettyPrint()

--- components/src/main/resources/META-INF/resources/js/butterfaces.js
+++ components/src/main/resources/META-INF/resources/js/butterfaces.js
@@ -45,11 +45,16 @@ function handlePrettyPrint() {
         var $firstPreChild = jQuery(this).find("pre span").first();
         var $lastPreChild = jQuery(this).find("pre span").last();
 
-        if (!$firstPreChild.html().trim()) {
-            $firstPreChild.remove();
+
+        if (!(typeof $firstPreChild.html() === "undefined")) {
+            if (!$firstPreChild.html().trim()) {
+                $firstPreChild.remove();
+            }
         }
-        if (!$lastPreChild.html().trim()) {
-            $lastPreChild.remove();
+        if (!(typeof $lastPreChild.html() === "undefined")) {
+            if (!$lastPreChild.html().trim()) {
+                $lastPreChild.remove();
+            }
         }
     });
 }
