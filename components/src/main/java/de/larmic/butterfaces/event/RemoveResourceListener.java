81f34af2acc5381e54034acccb20d860daca5e05
Lars Michaelis <mail@larmic.de>
2014-12-10 18:25:57 +0100

    BUT-162 Fix toolbar sizes

--- components/src/main/java/de/larmic/butterfaces/event/RemoveResourceListener.java
+++ components/src/main/java/de/larmic/butterfaces/event/RemoveResourceListener.java
@@ -8,6 +8,7 @@ import javax.faces.event.AbortProcessingException;
 import javax.faces.event.SystemEvent;
 import javax.faces.event.SystemEventListener;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -20,9 +21,7 @@ public class RemoveResourceListener implements SystemEventListener {
     public static final String CTX_PARAM_JQUERY = "de.larmic.butterfaces.provideJQuery";
     public static final String CTX_PARAM_BOOTSTRAP = "de.larmic.butterfaces.provideBootstrap";
     public static final String CTX_PARAM_PRETTYPRINT = "de.larmic.butterfaces.providePrettify";
-    public static final String CTX_PARAM_SORT_JQUERY = "de.larmic.butterfaces.sortJQueryResourceToFirstPosition";
     public static final String JQUERY_PREFIX_RESOURCE_IDENTIFIER = "jquery";
-    public static final String BOOTSTRAP_PREFIX_RESOURCE_IDENTIFIER = "bootstrap";
     public static final String PRETTYPRINT_PREFIX_RESOURCE_IDENTIFIER = "prettify";
 
     @Override
@@ -31,72 +30,76 @@ public class RemoveResourceListener implements SystemEventListener {
         final ExternalContext externalContext = context.getExternalContext();
         final boolean provideJQuery = this.readContextParameter(externalContext, CTX_PARAM_JQUERY);
         final boolean provideBootstrap = this.readContextParameter(externalContext, CTX_PARAM_BOOTSTRAP);
-        final boolean providePrettyprint = this.readContextParameter(externalContext, CTX_PARAM_PRETTYPRINT);
-        final boolean sortJQuery = this.readContextParameter(externalContext, CTX_PARAM_SORT_JQUERY);
+        final boolean providePrettyPrint = this.readContextParameter(externalContext, CTX_PARAM_PRETTYPRINT);
 
         final List<UIComponent> resources = new ArrayList<>(context.getViewRoot().getComponentResources(context, HEAD));
 
-        // put jquery on first position
-        if (provideJQuery && sortJQuery) {
-            final UIComponent jqueryResource = this.findAndRemoveJQueryResource(context, resources);
+        // configurable resources
+        UIComponent jqueryResource = null;
+        UIComponent bootstrapJSResource = null;
+        UIComponent bootstrapCSSResource = null;
+        UIComponent prettyPrintResource = null;
 
-            if (jqueryResource != null) {
-                context.getViewRoot().addComponentResource(context, jqueryResource, HEAD);
+        final Iterator<UIComponent> iterator = resources.iterator();
 
-                for (UIComponent resource : resources) {
-                    // remove it on some position
-                    context.getViewRoot().removeComponentResource(context, resource, HEAD);
-                    // add it on last position
-                    context.getViewRoot().addComponentResource(context, resource, HEAD);
-                }
-            }
-        }
+        // find all configurable resources
+        while (iterator.hasNext()) {
+            final UIComponent resource = iterator.next();
 
-        if (!provideJQuery || !provideBootstrap) {
-            // Fetch included resources list size
-            int i = resources.size() - 1;
-
-            while (i >= 0) {
-                // Fetch current resource from included resources list
-                final UIComponent resource = resources.get(i);
-                // Fetch resource library and resource name
-                final String resourceLibrary = (String) resource.getAttributes().get("library");
-                final String resourceName = (String) resource.getAttributes().get("name");
-
-                if (CONFIGURABLE_LIBRARY_NAME.equals(resourceLibrary)) {
-                    if (!provideJQuery && resourceName.startsWith(JQUERY_PREFIX_RESOURCE_IDENTIFIER)) {
-                        // Remove resource from view
-                        context.getViewRoot().removeComponentResource(context, resource, HEAD);
-                    }
-                    if (!provideBootstrap && resourceName.startsWith(BOOTSTRAP_PREFIX_RESOURCE_IDENTIFIER)) {
-                        // Remove resource from view
-                        context.getViewRoot().removeComponentResource(context, resource, HEAD);
-                    }
-                    if (!providePrettyprint && resourceName.startsWith(PRETTYPRINT_PREFIX_RESOURCE_IDENTIFIER)) {
-                        // Remove resource from view
-                        context.getViewRoot().removeComponentResource(context, resource, HEAD);
-                    }
-                }
-
-                i--;
-            }
-        }
-    }
-
-    private UIComponent findAndRemoveJQueryResource(final FacesContext context,
-                                                    final List<UIComponent> resources) {
-        for (UIComponent resource : resources) {
             final String resourceLibrary = (String) resource.getAttributes().get("library");
             final String resourceName = (String) resource.getAttributes().get("name");
-            if (CONFIGURABLE_LIBRARY_NAME.equals(resourceLibrary)
-                    && resourceName.startsWith(JQUERY_PREFIX_RESOURCE_IDENTIFIER)) {
-                context.getViewRoot().removeComponentResource(context, resource, HEAD);
-                resources.remove(resource);
-                return resource;
+
+            if (CONFIGURABLE_LIBRARY_NAME.equals(resourceLibrary)) {
+                if (resourceName.startsWith(JQUERY_PREFIX_RESOURCE_IDENTIFIER)) {
+                    jqueryResource = resource;
+                    iterator.remove();
+                } else if (resourceName.startsWith("bootstrap.min.css")) {
+                    bootstrapCSSResource = resource;
+                    iterator.remove();
+                } else if (resourceName.startsWith("bootstrap.min.js")) {
+                    bootstrapJSResource = resource;
+                    iterator.remove();
+                } else if (resourceName.startsWith(PRETTYPRINT_PREFIX_RESOURCE_IDENTIFIER)) {
+                    prettyPrintResource = resource;
+                    iterator.remove();
+                }
+            }
+
+            // to accelerate listener break if all configurable resources are found
+            if (jqueryResource != null && bootstrapCSSResource != null && bootstrapJSResource != null && prettyPrintResource != null) {
+                break;
             }
         }
 
-        return null;
+        // remove all resources from head
+        for (UIComponent resource : context.getViewRoot().getComponentResources(context, HEAD)) {
+            context.getViewRoot().removeComponentResource(context, resource, HEAD);
+        }
+
+        // add jquery
+        if (jqueryResource != null && provideJQuery) {
+            context.getViewRoot().addComponentResource(context, jqueryResource, HEAD);
+        }
+
+        // add bootstrap
+        if (provideBootstrap) {
+            if (bootstrapJSResource != null) {
+                context.getViewRoot().addComponentResource(context, bootstrapJSResource, HEAD);
+            }
+            if (bootstrapCSSResource != null) {
+                context.getViewRoot().addComponentResource(context, bootstrapCSSResource, HEAD);
+            }
+        }
+
+        // add pretty print
+        if (prettyPrintResource != null && providePrettyPrint) {
+            context.getViewRoot().addComponentResource(context, prettyPrintResource, HEAD);
+        }
+
+        // add remaining resources
+        for (UIComponent resource : resources) {
+            context.getViewRoot().addComponentResource(context, resource, HEAD);
+        }
     }
 
     private boolean readContextParameter(final ExternalContext externalContext, final String contextParameterName) {
