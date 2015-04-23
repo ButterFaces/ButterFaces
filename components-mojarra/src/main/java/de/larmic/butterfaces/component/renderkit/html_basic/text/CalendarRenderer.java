fa7e3d260ecb6fb73ef2fcedf1b602e49096ebaf
Lars Michaelis <mail@larmic.de>
2015-04-23 12:46:38 +0200

    BUT-255 Merge branch 'BUT-247-create-new-suggestion-box-compon'
    Conflicts:
    	components-mojarra/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/text/CalendarRenderer.java
    	components/src/main/resources/META-INF/butterfaces.taglib.xml

--- components-mojarra/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/text/CalendarRenderer.java
+++ components-mojarra/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/text/CalendarRenderer.java
@@ -1,18 +1,13 @@
-package de.larmic.butterfaces.component.renderkit.html_basic;
+package de.larmic.butterfaces.component.renderkit.html_basic.text;
 
-import java.io.IOException;
+import de.larmic.butterfaces.component.html.text.HtmlCalendar;
+import de.larmic.butterfaces.component.partrenderer.*;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 import javax.faces.render.FacesRenderer;
-
-import de.larmic.butterfaces.component.html.HtmlCalendar;
-import de.larmic.butterfaces.component.partrenderer.InnerComponentWrapperPartRenderer;
-import de.larmic.butterfaces.component.partrenderer.OuterComponentWrapperPartRenderer;
-import de.larmic.butterfaces.component.partrenderer.RenderUtils;
-import de.larmic.butterfaces.component.partrenderer.StringUtils;
-import de.larmic.butterfaces.component.partrenderer.TooltipPartRenderer;
+import java.io.IOException;
 
 @FacesRenderer(componentFamily = HtmlCalendar.COMPONENT_FAMILY, rendererType = HtmlCalendar.RENDERER_TYPE)
 public class CalendarRenderer extends AbstractTextRenderer<HtmlCalendar> {
@@ -83,7 +78,7 @@ public class CalendarRenderer extends AbstractTextRenderer<HtmlCalendar> {
     private String createJQueryPluginCallback(HtmlCalendar calendar) {
         final StringBuilder jQueryPluginCall = new StringBuilder();
         jQueryPluginCall.append("on(\"dp.change\", function (e) {");
-        jQueryPluginCall.append(RenderUtils.createJQueryBySelector(calendar.getClientId(), ".butter-input-component") + "trigger('keyup');");
+        jQueryPluginCall.append(RenderUtils.createJQueryBySelector(calendar.getClientId(), ".butter-input-component") + "trigger('change');");
         jQueryPluginCall.append("})");
         return jQueryPluginCall.toString();
     }
