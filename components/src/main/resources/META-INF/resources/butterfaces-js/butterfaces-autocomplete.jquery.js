bf2348d17e9d47402071244b17fb0d6b2f50f330
Stephan Zerhusen <s.zerhusen@byte-connect.de>
2015-06-23 14:25:12 +0200

    BUT-309 migrate JS plugin to base class structure

--- components/src/main/resources/META-INF/resources/butterfaces-js/butterfaces-autocomplete.jquery.js
+++ components/src/main/resources/META-INF/resources/butterfaces-js/butterfaces-autocomplete.jquery.js
@@ -23,54 +23,57 @@
 
             var self = this;
             self.$input.keyup(function (event) {
-                var testOnEvent = function (data) {
-                    var $autocomplete2 = $(document.getElementById(self.$autocomplete.attr('id')));
-                    if (data.status === 'success') {
-                        if (self.$input.data('data-test') === undefined) {
-                            if ($autocomplete2.has('li').size() > 0) {
-                                $autocomplete2
-                                        .css('display', 'inline')
-                                        .highlight(self.$input.val())
-                                        .find('li').on("click", function () {
-                                            self.$input
-                                                    .val($(this).attr("data-select-value"))
-                                                    .change()
-                                                    .data('data-test', 'true')
-                                                    .keyup()
-                                                    .focus();
-                                        });
-                            } else {
-                                $autocomplete2.css('display', 'none');
-                            }
-                        } else {
-                            $autocomplete2.css('display', 'none');
-                            self.$input.removeData('data-test');
-                        }
-                    }
-                };
-
                 if (self.$input.val().length === 0) {
                     event.stopPropagation();
                     event.preventDefault();
-                    $(document.getElementById(self.$autocomplete.attr('id'))).css('display', 'none');
-                    self.$input.removeData('data-test');
+                    $(document.getElementById(self.$autocomplete.attr("id"))).css("display", "none");
+                    self.$input.removeData("data-test");
                     return;
                 }
 
-                jsf.ajax.request(this, 'autocomplete', {
-                    'javax.faces.behavior.event': 'autocomplete',
-                    render: self.$autocomplete.attr('id'),
+                jsf.ajax.request(this, "autocomplete", {
+                    "javax.faces.behavior.event": "autocomplete",
+                    render: self.$autocomplete.attr("id"),
                     params: self.$input.val(),
-                    onevent: testOnEvent
+                    onevent: function (data) {
+                        self._testOnEvent(data);
+                    }
                 });
             });
 
             self.$input.blur(function () {
                 window.setTimeout(function () {
-                    $(document.getElementById(self.$autocomplete.attr('id')))
-                            .css('display', 'none');
+                    $(document.getElementById(self.$autocomplete.attr("id")))
+                            .css("display", "none");
                 }, 100);
             });
+        },
+
+        _testOnEvent: function (data) {
+            var self = this;
+            var $autocomplete2 = $(document.getElementById(self.$autocomplete.attr("id")));
+            if (data.status === "success") {
+                if (self.$input.data("data-test") === undefined) {
+                    if ($autocomplete2.has("li").size() > 0) {
+                        $autocomplete2
+                                .css("display", "inline")
+                                .highlight(self.$input.val())
+                                .find("li").on("click", function () {
+                                    self.$input
+                                            .val($(this).attr("data-select-value"))
+                                            .change()
+                                            .data("data-test", "true")
+                                            .keyup()
+                                            .focus();
+                                });
+                    } else {
+                        $autocomplete2.css("display", "none");
+                    }
+                } else {
+                    $autocomplete2.css("display", "none");
+                    self.$input.removeData("data-test");
+                }
+            }
         }
     });
 
