2125e38e7ef48ad9f85ac8c16e183e343b7aea54
Stephan Zerhusen <s.zerhusen@byte-connect.de>
2015-07-23 12:08:00 +0200

    Fixed: Sometimes the waiting panel doesn't disappear

--- components/src/main/resources/META-INF/resources/butterfaces-js/butterfaces-overlay.js
+++ components/src/main/resources/META-INF/resources/butterfaces-js/butterfaces-overlay.js
@@ -3,17 +3,20 @@ if (typeof butter === "undefined") {
 }
 butter.overlay = {};
 
+butter.overlay._isHiding = false;
+
 butter.overlay.show = function (/* object */ options) {
     if ($("body").find(".butter-component-overlay").length === 0) {
         var delay = 500;
-        var blockpage = true;
+        var isTransparentBlockingOverlayActive = true;
+        butter.overlay._isHiding = false;
 
         if (options !== undefined) {
             if (options.delay !== undefined) {
                 delay = options.delay;
             }
             if (options.blockpage !== undefined) {
-                blockpage = options.blockpage;
+                isTransparentBlockingOverlayActive = options.blockpage;
             }
         }
 
@@ -21,36 +24,42 @@ butter.overlay.show = function (/* object */ options) {
         var $overlay = $('<div class="butter-component-overlay"><div class="butter-component-spinner" /></div>');
         $("body").append($overlay);
 
-        if (blockpage) {
-            //console.log("butter.overlay.show - showing transparent overlay direcly if blockpage is true");
+        if (isTransparentBlockingOverlayActive) {
+            //console.log("butter.overlay.show - isTransparentBlockingOverlayActive is true, showing transparent overlay direcly");
             $overlay.show();
         }
 
         window.setTimeout(function () {
-            if (!blockpage) {
-                //console.log("butter.overlay.show - blockpage is false, showing transparent overlay after delay");
+            if (!isTransparentBlockingOverlayActive) {
+                //console.log("butter.overlay.show - deferred: isTransparentBlockingOverlayActive is false, showing transparent overlay after delay");
                 $overlay.show();
             }
-            //console.log("butter.overlay.show - starting animation to make overlay intransparent");
-            $overlay
-                    .stop(true)
-                    .animate({
-                        opacity: 1
-                    }, 300, function () {
-                        //console.log("butter.overlay.show - animation ended to make overlay intransparent");
-                    });
+
+            if (!butter.overlay._isHiding) {
+                //console.log("butter.overlay.show - deferred: starting animation to make overlay intransparent");
+                $overlay
+                        .stop(true)
+                        .animate({
+                            opacity: 1
+                        }, 300, function () {
+                            //console.log("butter.overlay.show - deferred: animation ended to make overlay intransparent");
+                        });
+            }
         }, delay);
+
     }
 };
 
 butter.overlay.hide = function () {
     //console.log("butter.overlay.hide - starting animation to make overlay transparent");
-    $("body > .butter-component-overlay")
+    var $overlay = $("body > .butter-component-overlay");
+    butter.overlay._isHiding = true;
+    $overlay
             .stop(true)
             .animate({
                 opacity: 0
             }, 300, function () {
-                //console.log("butter.overlay.hide - animation ended to make overlay transparent, removing overlay from DOM");
                 $(this).remove();
+                //console.log("butter.overlay.hide - animation ended to make overlay transparent, OVERLAY REMOVED");
             });
 };
