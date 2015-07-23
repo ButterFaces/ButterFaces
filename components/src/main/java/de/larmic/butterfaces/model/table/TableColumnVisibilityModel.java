575a6c22208b05db17b091934ef568bec33a25fb
Lars Michaelis <mail@larmic.de>
2015-07-23 12:42:36 +0200

    fixed javadoc

--- components/src/main/java/de/larmic/butterfaces/model/table/TableColumnVisibilityModel.java
+++ components/src/main/java/de/larmic/butterfaces/model/table/TableColumnVisibilityModel.java
@@ -7,10 +7,14 @@ public interface TableColumnVisibilityModel {
 
     /**
      * Updates table column visibility. At all times it contains complete visibility information.
+     *
+     * @param visibility dto
      */
     void update(final TableColumnVisibility visibility);
 
     /**
+     * @param tableUniqueIdentifier  table unique identifier (component client if if unique identifier is empty)
+     * @param columnUniqueIdentifier column unique identifier (component client if if unique identifier is empty)
      * @return null if column identifier is not known, table identifier is not matching or no visibility is set.
      */
     Boolean isColumnHidden(final String tableUniqueIdentifier, final String columnUniqueIdentifier);
--- components/src/main/java/de/larmic/butterfaces/model/table/TableModel.java
+++ components/src/main/java/de/larmic/butterfaces/model/table/TableModel.java
@@ -7,7 +7,7 @@ package de.larmic.butterfaces.model.table;
 public interface TableModel {
 
     /**
-     * Only works if at least one <f:ajax /> is used on table toolbar component. Maybe it is better to create custom
+     * Only works if at least one f:ajax is used on table toolbar component. Maybe it is better to create custom
      * ajax event type for this model (like both other models have) but up to know no specific event type is needed.
      *
      * @return null if no sort model is used. This means sort columns is not available.
@@ -15,14 +15,14 @@ public interface TableModel {
     TableRowSortingModel getTableRowSortingModel();
 
     /**
-     * Only works if <f:ajax event="toggle" /> is used on table toolbar component.
+     * Only works if f:ajax event="toggle" is used on table toolbar component.
      *
      * @return null if no column visibility (show and hide) is used.
      */
     TableColumnVisibilityModel getTableColumnVisibilityModel();
 
     /**
-     * Only works if <f:ajax event="order" /> is used on table toolbar component.
+     * Only works if f:ajax event="order" is used on table toolbar component.
      *
      * @return null if no column ordering is used.
      */
