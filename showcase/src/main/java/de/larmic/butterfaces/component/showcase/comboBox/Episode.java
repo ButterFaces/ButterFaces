f96914e54b6c416add098061e3e474aba7a9c75d
Lars Michaelis <mail@larmic.de>
2015-07-27 18:32:56 +0200

    BUT-343 update combobox showcase using templating

---
+++ showcase/src/main/java/de/larmic/butterfaces/component/showcase/comboBox/Episode.java
@@ -0,0 +1,52 @@
+package de.larmic.butterfaces.component.showcase.comboBox;
+
+/**
+ * Created by stephan on 13.07.15.
+ */
+public class Episode {
+    private int numberInSeries;
+    private String title;
+    private String directedBy;
+    private String writtenBy;
+    private String originalAirDate;
+
+    public int getNumberInSeries() {
+        return numberInSeries;
+    }
+
+    public void setNumberInSeries(int numberInSeries) {
+        this.numberInSeries = numberInSeries;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirectedBy() {
+        return directedBy;
+    }
+
+    public void setDirectedBy(String directedBy) {
+        this.directedBy = directedBy;
+    }
+
+    public String getWrittenBy() {
+        return writtenBy;
+    }
+
+    public void setWrittenBy(String writtenBy) {
+        this.writtenBy = writtenBy;
+    }
+
+    public String getOriginalAirDate() {
+        return originalAirDate;
+    }
+
+    public void setOriginalAirDate(String originalAirDate) {
+        this.originalAirDate = originalAirDate;
+    }
+}
--- showcase/src/main/java/de/larmic/butterfaces/component/showcase/type/ComboBoxValueType.java
+++ showcase/src/main/java/de/larmic/butterfaces/component/showcase/type/ComboBoxValueType.java
@@ -1,7 +1,7 @@
 package de.larmic.butterfaces.component.showcase.type;
 
 public enum ComboBoxValueType {
-	STRING("string"), ENUM("enum"), OBJECT("object");
+	STRING("string"), ENUM("enum"), OBJECT("object"), TEMPLATE("template");
 	public final String label;
 
 	private ComboBoxValueType(final String label) {
