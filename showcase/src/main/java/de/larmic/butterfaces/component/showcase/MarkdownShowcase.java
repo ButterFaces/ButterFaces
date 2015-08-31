161a1bc986b341a01a8527e046aa11dc71c5d168
Lars Michaelis <mail@larmic.de>
2015-08-31 14:53:56 +0200

    BUT-373 update markdown showcase

---
+++ showcase/src/main/java/de/larmic/butterfaces/component/showcase/MarkdownShowcase.java
@@ -0,0 +1,93 @@
+package de.larmic.butterfaces.component.showcase;
+
+import de.larmic.butterfaces.component.partrenderer.StringUtils;
+import de.larmic.butterfaces.component.showcase.example.AbstractCodeExample;
+import de.larmic.butterfaces.component.showcase.example.XhtmlCodeExample;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+@SuppressWarnings("serial")
+public class MarkdownShowcase extends AbstractInputShowcase implements Serializable {
+
+    private Integer maxLength;
+    private boolean autoFocus;
+
+    private String placeholder = DEFAULT_TEXT_PLACEHOLDER;
+
+    @Override
+    protected Object initValue() {
+        return null;
+    }
+
+    @Override
+    public String getReadableValue() {
+        return (String) this.getValue();
+    }
+
+    @Override
+    public void buildCodeExamples(final List<AbstractCodeExample> codeExamples) {
+        final XhtmlCodeExample xhtmlCodeExample = new XhtmlCodeExample(false);
+
+        xhtmlCodeExample.appendInnerContent("        <b:markdown id=\"input\"");
+        xhtmlCodeExample.appendInnerContent("                    label=\"" + this.getLabel() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    hideLabel=\"" + isHideLabel() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    value=\"" + this.getValue() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    tooltip=\"" + this.getTooltip() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    styleClass=\"" + this.getStyleClass() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    readonly=\"" + this.isReadonly() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    required=\"" + this.isRequired() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    disabled=\"" + this.isDisabled() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    placeholder=\"" + placeholder + "\"");
+        xhtmlCodeExample.appendInnerContent("                    autoFocus=\"" + this.isAutoFocus() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    rendered=\"" + this.isRendered() + "\">");
+
+        if (this.getMaxLength() != null) {
+            xhtmlCodeExample.appendInnerContent("                    maxLength=\"" + this.getMaxLength() + "\"");
+        }
+
+        this.addAjaxTag(xhtmlCodeExample, "keyup");
+
+        if (StringUtils.isNotEmpty(getTooltip())) {
+            xhtmlCodeExample.appendInnerContent("            <b:tooltip>");
+            xhtmlCodeExample.appendInnerContent("                " + getTooltip());
+            xhtmlCodeExample.appendInnerContent("            </b:tooltip>");
+        }
+
+        xhtmlCodeExample.appendInnerContent("        </b:markdown>", false);
+
+        this.addOutputExample(xhtmlCodeExample);
+
+        codeExamples.add(xhtmlCodeExample);
+
+        generateDemoCSS(codeExamples);
+    }
+
+    public Integer getMaxLength() {
+        return this.maxLength;
+    }
+
+    public void setMaxLength(final Integer maxLength) {
+        this.maxLength = maxLength;
+    }
+
+    public String getPlaceholder() {
+        return this.placeholder;
+    }
+
+    public void setPlaceholder(final String placeholder) {
+        this.placeholder = placeholder;
+    }
+
+    public boolean isAutoFocus() {
+        return autoFocus;
+    }
+
+    public void setAutoFocus(boolean autoFocus) {
+        this.autoFocus = autoFocus;
+    }
+}
