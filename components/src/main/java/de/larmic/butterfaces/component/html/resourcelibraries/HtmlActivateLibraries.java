0d3269afaee3210d46fe90083036c85006630269
Lars Michaelis <mail@larmic.de>
2015-09-02 08:14:49 +0200

    BUT-377 Fixed markdown invalid css

--- components/src/main/java/de/larmic/butterfaces/component/html/resourcelibraries/HtmlActivateLibraries.java
+++ components/src/main/java/de/larmic/butterfaces/component/html/resourcelibraries/HtmlActivateLibraries.java
@@ -29,6 +29,7 @@ import javax.faces.component.UIComponentBase;
         @ResourceDependency(library = "butterfaces-css", name = "butterfaces-autocomplete.css", target = "head"),
         @ResourceDependency(library = "butterfaces-css", name = "butterfaces-dropdownlist.css", target = "head"),
         @ResourceDependency(library = "butterfaces-css", name = "butterfaces-labeled-container.css", target = "head"),
+        @ResourceDependency(library = "butterfaces-css", name = "butterfaces-markdown.css", target = "head"),
         @ResourceDependency(library = "butterfaces-js", name = "butterfaces-bootstrap-fixes.jquery.js", target = "head"),
         @ResourceDependency(library = "butterfaces-js", name = "butterfaces-fixed.js", target = "head"),
         @ResourceDependency(library = "butterfaces-js", name = "butterfaces-overlay.js", target = "head"),
