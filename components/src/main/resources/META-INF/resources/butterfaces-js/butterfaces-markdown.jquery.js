2aa1f69e4fbaa343bdf790a2c0f8e45b75a5fc2a
Lars Michaelis <mail@larmic.de>
2015-09-03 08:36:17 +0200

    BUT-379 Fixed markdown quote

--- components/src/main/resources/META-INF/resources/butterfaces-js/butterfaces-markdown.jquery.js
+++ components/src/main/resources/META-INF/resources/butterfaces-js/butterfaces-markdown.jquery.js
@@ -4,8 +4,9 @@
         var root = $(this);
 
         var $readonlyMarkdown = root.find('.butter-component-value-readonly-wrapper');
-        var markdownText = $readonlyMarkdown.html();
+        var markdownText = $readonlyMarkdown.html().replace('&gt;', '>');
         var markdownTextToHtml = markdown.toHTML(markdownText);
+
         $readonlyMarkdown.empty();
         $readonlyMarkdown.append(markdownTextToHtml);
     };
