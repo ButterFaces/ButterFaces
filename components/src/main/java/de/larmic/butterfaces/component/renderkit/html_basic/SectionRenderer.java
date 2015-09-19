01000eb9f8470d4c9865b8cf27aafb3c9955ce8d
Lars Michaelis <mail@larmic.de>
2015-09-19 13:24:21 +0200

    BUT-389 Convert SectionRenderer.java to HtmlBasicRenderer.java

--- components/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/SectionRenderer.java
+++ components/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/SectionRenderer.java
@@ -1,6 +1,6 @@
 package de.larmic.butterfaces.component.renderkit.html_basic;
 
-import de.larmic.butterfaces.component.base.renderer.HtmlDeprecatedBasicRenderer;
+import de.larmic.butterfaces.component.base.renderer.HtmlBasicRenderer;
 import de.larmic.butterfaces.component.html.HtmlSection;
 import de.larmic.butterfaces.component.partrenderer.StringUtils;
 
@@ -14,13 +14,11 @@ import java.io.IOException;
  * Created by larmic on 31.07.14.
  */
 @FacesRenderer(componentFamily = HtmlSection.COMPONENT_FAMILY, rendererType = HtmlSection.RENDERER_TYPE)
-public class SectionRenderer extends HtmlDeprecatedBasicRenderer {
+public class SectionRenderer extends HtmlBasicRenderer {
 
     @Override
     public void encodeBegin(final FacesContext context, final UIComponent component) throws IOException {
-        rendererParamsNotNull(context, component);
-
-        if (!shouldEncode(component)) {
+        if (!component.isRendered()) {
             return;
         }
 
@@ -79,9 +77,7 @@ public class SectionRenderer extends HtmlDeprecatedBasicRenderer {
 
     @Override
     public void encodeEnd(final FacesContext context, final UIComponent component) throws IOException {
-        rendererParamsNotNull(context, component);
-
-        if (!shouldEncode(component)) {
+        if (!component.isRendered()) {
             return;
         }
 
