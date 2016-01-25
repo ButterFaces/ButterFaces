0f8d5cf15d41379161bb02b9e485c8c595582c3b
Lars Michaelis <mail@larmic.de>
2016-01-25 18:14:24 +0100

    BUT-457 treeBox: add todo

--- components/src/main/resources/META-INF/resources/butterfaces-ts/butterfaces-treebox.ts
+++ components/src/main/resources/META-INF/resources/butterfaces-ts/butterfaces-treebox.ts
@@ -5,6 +5,8 @@ module ButterFaces {
     export class TreeBox {
         public static removeTrivialTreeDropDown(treeBoxId:string) {
             $('.tr-dropdown[data-tree-box-id=' + treeBoxId + ']').remove();
+
+            // TODO remove unbinded dropdowns
         }
     }
 
