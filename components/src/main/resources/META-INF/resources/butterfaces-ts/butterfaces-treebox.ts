40fc71495a53687708442d911cbdf5b5b7d4b2be
Lars Michaelis <mail@larmic.de>
2016-01-26 10:12:23 +0100

    BUT-457 treeBox: remove unassociated tr-dropdown

--- components/src/main/resources/META-INF/resources/butterfaces-ts/butterfaces-treebox.ts
+++ components/src/main/resources/META-INF/resources/butterfaces-ts/butterfaces-treebox.ts
@@ -1,13 +1,31 @@
-/// <reference path="definitions/external/jquery/jquery.d.ts" />
-
 module ButterFaces {
 
     export class TreeBox {
-        public static removeTrivialTreeDropDown(treeBoxId:string) {
-            $('.tr-dropdown[data-tree-box-id=' + treeBoxId + ']').remove();
 
-            // TODO remove unbinded dropdowns
+        public static removeTrivialTreeDropDown(treeBoxId:string) {
+            this.removeTrivialTreeDropDownById(treeBoxId);
+            this.removeUnAssociatedTrivialTreeDropDowns();
         }
+
+        private static removeUnAssociatedTrivialTreeDropDowns() {
+            var dropdowns = document.querySelectorAll('.tr-dropdown[data-tree-box-id]');
+
+            Array.prototype.slice.call(dropdowns, 0).forEach(dropdown => {
+                var dropdownId = dropdown.getAttribute('data-tree-box-id');
+                if (document.querySelectorAll('.butter-component-treebox[data-tree-box-id=' + dropdownId + ']').length == 0) {
+                    dropdown.parentNode.removeChild(dropdown);
+                }
+            });
+        }
+
+        private static removeTrivialTreeDropDownById(treeBoxId) {
+            // in jQuery: $('.tr-dropdown[data-tree-box-id=' + treeBoxId + ']').remove();
+            // plain javascript:
+            var dropdowns = document.querySelectorAll('.tr-dropdown[data-tree-box-id=' + treeBoxId + ']');
+            Array.prototype.slice.call(dropdowns, 0).forEach(dropdown => {
+                dropdown.parentNode.removeChild(dropdown);
+            });
+        };
     }
 
 }
