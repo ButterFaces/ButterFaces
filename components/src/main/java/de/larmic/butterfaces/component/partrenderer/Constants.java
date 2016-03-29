04f9e744695d9418e792158768a303f8c6808d99
Lars Michaelis <mail@larmic.de>
2016-03-29 18:12:51 +0200

    #12 documentation

--- components/src/main/java/de/larmic/butterfaces/component/partrenderer/Constants.java
+++ components/src/main/java/de/larmic/butterfaces/component/partrenderer/Constants.java
@@ -1,7 +1,12 @@
+/*
+ * Copyright Lars Michaelis and Stephan Zerhusen 2016.
+ * Distributed under the MIT License.
+ * (See accompanying file README.md file or copy at http://opensource.org/licenses/MIT)
+ */
 package de.larmic.butterfaces.component.partrenderer;
 
 /**
- * Created by larmic on 29.08.14.
+ * @author Lars Michaelis
  */
 public class Constants {
 
