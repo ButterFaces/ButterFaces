426e48b858cec946cc398ed67d9ec72ee6cfdd2b
Lars Michaelis <mail@larmic.de>
2016-04-12 18:05:39 +0200

    #23 remove mojarra from showcase

--- showcase/src/main/java/de/larmic/butterfaces/component/showcase/DependencyExample.java
+++ showcase/src/main/java/de/larmic/butterfaces/component/showcase/DependencyExample.java
@@ -15,17 +15,13 @@ public class DependencyExample implements Serializable {
     public String getButterFacesMavenDependency() {
         return createDependency("components");    }
 
-    public String getButterFacesMojarraMavenDependency() {
-        return createDependency("components-mojarra");
-    }
-
     private String createDependency(String artifactId) {
         final StringBuilder sb = new StringBuilder();
 
         sb.append("<dependency>\n");
-        sb.append(" <groupId>de.larmic.butterfaces</groupId>\n");
-        sb.append(" <artifactId>" + artifactId + "</artifactId>\n");
-        sb.append(" <version>" + version.getLastestReleaseVersion() + "</version>\n");
+        sb.append("   <groupId>de.larmic.butterfaces</groupId>\n");
+        sb.append("   <artifactId>" + artifactId + "</artifactId>\n");
+        sb.append("   <version>" + version.getLastestReleaseVersion() + "</version>\n");
         sb.append("</dependency>");
 
         return sb.toString();
