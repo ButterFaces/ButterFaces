91ace05a5f78d6ee586cd40f79a0bfc9a1e918b5
Lars Michaelis <mail@larmic.de>
2016-04-13 10:32:38 +0200

    update some list of objects showcase

--- showcase/src/main/java/de/larmic/butterfaces/component/showcase/radioBox/RadioBoxShowcase.java
+++ showcase/src/main/java/de/larmic/butterfaces/component/showcase/radioBox/RadioBoxShowcase.java
@@ -12,69 +12,71 @@ import javax.faces.model.SelectItem;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Named
 @ViewScoped
 @SuppressWarnings("serial")
 public class RadioBoxShowcase extends AbstractInputShowcase implements Serializable {
 
-	private RadioBoxExampleType exampleType = RadioBoxExampleType.STRING;
+    private RadioBoxExampleType exampleType = RadioBoxExampleType.STRING;
     private RadioBoxLayoutType radioBoxLayoutType = RadioBoxLayoutType.LINE_DIRECTION;
 
-	private final List<Foo> foos = new ArrayList<>();
-	private final List<FooType> enums = new ArrayList<>();
-	private final List<String> strings = new ArrayList<>();
+    private final List<Foo> foos = new ArrayList<>();
+    private final List<FooType> enums = new ArrayList<>();
+    private final List<String> strings = new ArrayList<>();
 
-	public RadioBoxShowcase() {
-		this.initFoos();
-		this.initStrings();
-		this.initEnums();
-	}
+    public RadioBoxShowcase() {
+        this.initFoos();
+        this.initStrings();
+        this.initEnums();
+    }
 
-	@Override
-	protected Object initValue() {
-		return null;
-	}
+    @Override
+    protected Object initValue() {
+        return null;
+    }
 
-	@Override
-	public Object getValue() {
-		if (super.getValue() != null) {
-			return super.getValue();
-		}
+    @Override
+    public Object getValue() {
+        if (super.getValue() != null) {
+            return super.getValue();
+        }
 
-		return "(item is null)";
-	}
+        return "(item is null)";
+    }
 
-	@Override
-	public void buildCodeExamples(final List<AbstractCodeExample> codeExamples) {
-		final XhtmlCodeExample xhtmlCodeExample = new XhtmlCodeExample(false);
+    @Override
+    public void buildCodeExamples(final List<AbstractCodeExample> codeExamples) {
+        final XhtmlCodeExample xhtmlCodeExample = new XhtmlCodeExample(false);
 
-		xhtmlCodeExample.appendInnerContent("        <b:radioBox id=\"input\"");
-		xhtmlCodeExample.appendInnerContent("                    label=\"" + this.getLabel() + "\"");
-		xhtmlCodeExample.appendInnerContent("                    hideLabel=\"" + isHideLabel() + "\"");
-		xhtmlCodeExample.appendInnerContent("                    value=\"#{myBean.selectedValue}\"");
-		xhtmlCodeExample.appendInnerContent("                    values=\"#{myBean.values}\"");
-		xhtmlCodeExample.appendInnerContent("                    styleClass=\"" + this.getStyleClass() + "\"");
-		xhtmlCodeExample.appendInnerContent("                    readonly=\"" + this.isReadonly() + "\"");
-		xhtmlCodeExample.appendInnerContent("                    required=\"" + this.isRequired() + "\"");
-		xhtmlCodeExample.appendInnerContent("                    disabled=\"" + this.isDisabled() + "\"");
-		//xhtmlCodeExample.appendInnerContent("                    layout=\"" + radioBoxLayoutType.label + "\"");
-		xhtmlCodeExample.appendInnerContent("                    rendered=\"" + this.isRendered() + "\">");
+        xhtmlCodeExample.appendInnerContent("        <b:radioBox id=\"input\"");
+        xhtmlCodeExample.appendInnerContent("                    label=\"" + this.getLabel() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    hideLabel=\"" + isHideLabel() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    value=\"#{myBean.selectedValue}\"");
+        xhtmlCodeExample.appendInnerContent("                    values=\"#{myBean.values}\"");
+        xhtmlCodeExample.appendInnerContent("                    styleClass=\"" + this.getStyleClass() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    readonly=\"" + this.isReadonly() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    required=\"" + this.isRequired() + "\"");
+        xhtmlCodeExample.appendInnerContent("                    disabled=\"" + this.isDisabled() + "\"");
+        //xhtmlCodeExample.appendInnerContent("                    layout=\"" + radioBoxLayoutType.label + "\"");
+        xhtmlCodeExample.appendInnerContent("                    rendered=\"" + this.isRendered() + "\">");
 
-		this.addAjaxTag(xhtmlCodeExample, "change");
+        this.addAjaxTag(xhtmlCodeExample, "change");
 
-		if (StringUtils.isNotEmpty(getTooltip())) {
-			xhtmlCodeExample.appendInnerContent("            <b:tooltip>");
-			xhtmlCodeExample.appendInnerContent("                " + getTooltip());
-			xhtmlCodeExample.appendInnerContent("            </b:tooltip>");
-		}
+        if (StringUtils.isNotEmpty(getTooltip())) {
+            xhtmlCodeExample.appendInnerContent("            <b:tooltip>");
+            xhtmlCodeExample.appendInnerContent("                " + getTooltip());
+            xhtmlCodeExample.appendInnerContent("            </b:tooltip>");
+        }
 
-		xhtmlCodeExample.appendInnerContent("        </b:radioBox>", false);
+        xhtmlCodeExample.appendInnerContent("        </b:radioBox>", false);
 
-		this.addOutputExample(xhtmlCodeExample);
+        this.addOutputExample(xhtmlCodeExample);
 
-		codeExamples.add(xhtmlCodeExample);
+        codeExamples.add(xhtmlCodeExample);
 
         if (RadioBoxExampleType.STRING.equals(this.exampleType)) {
             codeExamples.add(new RadioBoxListOfStringsMyBeanExample());
@@ -86,65 +88,63 @@ public class RadioBoxShowcase extends AbstractInputShowcase implements Serializa
             codeExamples.add(new RadioBoxFooExample());
         }
 
-		generateDemoCSS(codeExamples);
-	}
+        generateDemoCSS(codeExamples);
+    }
 
 
-	@Override
-	public String getReadableValue() {
-		if (super.getValue() != null) {
-			if (super.getValue() instanceof Foo) {
-				return ((Foo) super.getValue()).getValue();
-			} else if (super.getValue() instanceof FooType) {
-				return ((FooType) super.getValue()).getLabel();
-			} else if (super.getValue() instanceof SelectItem) {
+    @Override
+    public String getReadableValue() {
+        if (super.getValue() != null) {
+            if (super.getValue() instanceof Foo) {
+                return ((Foo) super.getValue()).getValue();
+            } else if (super.getValue() instanceof FooType) {
+                return ((FooType) super.getValue()).getLabel();
+            } else if (super.getValue() instanceof SelectItem) {
                 return ((SelectItem) super.getValue()).getLabel();
             }
 
-			return (String) super.getValue();
-		}
+            return (String) super.getValue();
+        }
 
-		return "(item is null)";
-	}
+        return "(item is null)";
+    }
 
-	public List getValues() {
-		switch (this.exampleType) {
-		case OBJECT:
-			return this.foos;
-		case ENUM:
-			return this.enums;
-		default:
-			return this.strings;
-		}
-	}
+    public List getValues() {
+        switch (this.exampleType) {
+            case OBJECT:
+                return this.foos;
+            case ENUM:
+                return this.enums;
+            default:
+                return this.strings;
+        }
+    }
 
-	public List<SelectItem> getExampleTypes() {
-		final List<SelectItem> items = new ArrayList<>();
+    public List<SelectItem> getExampleTypes() {
+        final List<SelectItem> items = new ArrayList<>();
 
-		for (final RadioBoxExampleType type : RadioBoxExampleType.values()) {
-            if (!RadioBoxExampleType.TEMPLATE.equals(type)) {
-                items.add(new SelectItem(type, type.label));
-            }
-		}
-		return items;
-	}
+        for (final RadioBoxExampleType type : RadioBoxExampleType.values()) {
+            items.add(new SelectItem(type, type.label));
+        }
+        return items;
+    }
 
     public List<SelectItem> getRadioLayoutTypes() {
-		final List<SelectItem> items = new ArrayList<>();
+        final List<SelectItem> items = new ArrayList<>();
 
-		for (final RadioBoxLayoutType type : RadioBoxLayoutType.values()) {
-			items.add(new SelectItem(type, type.label));
-		}
-		return items;
-	}
+        for (final RadioBoxLayoutType type : RadioBoxLayoutType.values()) {
+            items.add(new SelectItem(type, type.label));
+        }
+        return items;
+    }
 
-	public RadioBoxExampleType getExampleType() {
-		return this.exampleType;
-	}
+    public RadioBoxExampleType getExampleType() {
+        return this.exampleType;
+    }
 
-	public void setExampleType(final RadioBoxExampleType exampleType) {
-		this.exampleType = exampleType;
-	}
+    public void setExampleType(final RadioBoxExampleType exampleType) {
+        this.exampleType = exampleType;
+    }
 
     public RadioBoxLayoutType getRadioBoxLayoutType() {
         return radioBoxLayoutType;
@@ -158,15 +158,15 @@ public class RadioBoxShowcase extends AbstractInputShowcase implements Serializa
         this.foos.add(new Foo("fooKey1", "fooValue1"));
         this.foos.add(new Foo("fooKey2", "fooValue2"));
         this.foos.add(new Foo("fooKey3", "fooValue3"));
-	}
+    }
 
-	private void initEnums() {
+    private void initEnums() {
         Collections.addAll(this.enums, FooType.values());
-	}
+    }
 
-	private void initStrings() {
-		this.strings.add("Year 2000");
-		this.strings.add("Year 2010");
-		this.strings.add("Year 2020");
-	}
+    private void initStrings() {
+        this.strings.add("Year 2000");
+        this.strings.add("Year 2010");
+        this.strings.add("Year 2020");
+    }
 }
--- showcase/src/main/java/de/larmic/butterfaces/component/showcase/type/RadioBoxExampleType.java
+++ showcase/src/main/java/de/larmic/butterfaces/component/showcase/type/RadioBoxExampleType.java
@@ -3,8 +3,8 @@ package de.larmic.butterfaces.component.showcase.type;
 public enum RadioBoxExampleType {
 	STRING("string"),
 	ENUM("enum"),
-	OBJECT("object"),
-	TEMPLATE("template");
+	OBJECT("object");
+
 	public final String label;
 
 	RadioBoxExampleType(final String label) {
