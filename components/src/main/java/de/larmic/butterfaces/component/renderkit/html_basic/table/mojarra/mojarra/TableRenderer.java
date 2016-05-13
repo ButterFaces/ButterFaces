26e7dc0b588232eaa3a7341859f18bc34161da18
Lars Michaelis <mail@larmic.de>
2016-05-13 07:29:39 +0200

    #45 new table: remove some unused code

--- components/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/table/mojarra/mojarra/TableRenderer.java
+++ components/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/table/mojarra/mojarra/TableRenderer.java
@@ -1,14 +1,10 @@
 package de.larmic.butterfaces.component.renderkit.html_basic.table.mojarra.mojarra;
 
-import javax.faces.component.UIColumn;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIData;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by larmic on 12.05.16.
@@ -53,19 +49,11 @@ public abstract class TableRenderer extends BaseTableRenderer {
 
         ResponseWriter writer = context.getResponseWriter();
 
-        // Check if any columns are being rendered, if not
-        // render the minimal markup and exit
-        TableMetaInfo info = getMetaInfo(context, data);
-        if (info.columns.isEmpty()) {
-            renderEmptyTableBody(writer, data);
-            return;
-        }
         // Iterate over the rows of data that are provided
         int processed = 0;
         int rowIndex = data.getFirst() - 1;
         int rows = data.getRows();
         renderTableBodyStart(context, component, writer);
-        boolean renderedRow = false;
         while (true) {
 
             // Have we displayed the requested number of rows?
@@ -86,14 +74,9 @@ public abstract class TableRenderer extends BaseTableRenderer {
 
             // Render the ending of this row
             renderRowEnd(context, component, writer);
-            renderedRow = true;
 
         }
 
-        // fill an empty tbody, if no row has been rendered
-        if (!renderedRow) {
-            this.renderEmptyTableRow(writer, data);
-        }
         renderTableBodyEnd(context, component, writer);
 
         // Clean up after ourselves
@@ -118,59 +101,6 @@ public abstract class TableRenderer extends BaseTableRenderer {
 
     @Override
     public boolean getRendersChildren() {
-
         return true;
-
-    }
-
-    // ------------------------------------------------------- Private Methods
-
-    private void renderEmptyTableBody(final ResponseWriter writer,
-                                      final UIComponent component)
-            throws IOException {
-
-        writer.startElement("tbody", component);
-        this.renderEmptyTableRow(writer, component);
-        writer.endElement("tbody");
-
-    }
-
-    private void renderEmptyTableRow(final ResponseWriter writer,
-                                     final UIComponent component)
-            throws IOException {
-
-        writer.startElement("tr", component);
-        List<UIColumn> columns = getColumns(component);
-        for (UIColumn column : columns) {
-            if (column.isRendered()) {
-                writer.startElement("td", component);
-                writer.endElement("td");
-            }
-        }
-        writer.endElement("tr");
-    }
-
-    /**
-     * <p>Return an Iterator over the <code>UIColumn</code> children of the
-     * specified <code>UIData</code> that have a <code>rendered</code> property
-     * of <code>true</code>.</p>
-     *
-     * @param table the table from which to extract children
-     * @return the List of all UIColumn children
-     */
-    private List<UIColumn> getColumns(UIComponent table) {
-        int childCount = table.getChildCount();
-        if (childCount > 0) {
-            List<UIColumn> results =
-                    new ArrayList<UIColumn>(childCount);
-            for (UIComponent kid : table.getChildren()) {
-                if ((kid instanceof UIColumn) && kid.isRendered()) {
-                    results.add((UIColumn) kid);
-                }
-            }
-            return results;
-        } else {
-            return Collections.emptyList();
-        }
     }
 }
