8f0726ee7a6f3a8192c6ec9da8b3633a62a5fb87
Lars Michaelis <mail@larmic.de>
2016-05-13 07:46:38 +0200

    #45 new table: merge MojarraTableRenderer.java and TableRenderer.java

--- components/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/table/mojarra/TableRenderer.java
+++ components/src/main/java/de/larmic/butterfaces/component/renderkit/html_basic/table/mojarra/TableRenderer.java
@@ -3,14 +3,17 @@ package de.larmic.butterfaces.component.renderkit.html_basic.table.mojarra;
 import de.larmic.butterfaces.component.base.renderer.HtmlBasicRenderer;
 import de.larmic.butterfaces.component.behavior.JsfAjaxRequest;
 import de.larmic.butterfaces.component.html.table.HtmlColumnNew;
+import de.larmic.butterfaces.component.html.table.HtmlTable;
 import de.larmic.butterfaces.component.html.table.HtmlTableNew;
 import de.larmic.butterfaces.component.partrenderer.RenderUtils;
+import de.larmic.butterfaces.component.renderkit.html_basic.table.cache.TableMetaInfo;
 import de.larmic.butterfaces.event.TableSingleSelectionListener;
 import de.larmic.butterfaces.model.table.SortType;
 import de.larmic.butterfaces.resolver.ClientBehaviorResolver;
 import de.larmic.butterfaces.resolver.WebXmlParameters;
 import de.larmic.butterfaces.util.StringUtils;
 
+import javax.faces.component.UIColumn;
 import javax.faces.component.UIComponent;
 import javax.faces.component.behavior.AjaxBehavior;
 import javax.faces.component.behavior.ClientBehavior;
@@ -27,7 +30,7 @@ import java.util.Map;
  * Created by larmic on 10.09.14.
  */
 @FacesRenderer(componentFamily = HtmlTableNew.COMPONENT_FAMILY, rendererType = HtmlTableNew.RENDERER_TYPE)
-public class TableRenderer extends MojarraTableRenderer {
+public class TableRenderer extends de.larmic.butterfaces.component.renderkit.html_basic.table.mojarra.mojarra.TableRenderer {
 
     private boolean hasColumnWidthSet;
     // dirty: method renderRowStart does not have a rowIndex parameter but it should.
@@ -195,6 +198,76 @@ public class TableRenderer extends MojarraTableRenderer {
         rowIndex++;
     }
 
+    protected void renderRow(FacesContext context,
+                             UIComponent table,
+                             UIComponent child,
+                             ResponseWriter writer) throws IOException {
+
+        // Iterate over the child UIColumn components for each row
+        final TableMetaInfo info = getMetaInfo(context, table);
+        info.newRow();
+
+        int columnNumber = 0;
+
+        for (UIColumn column : info.columns) {
+
+            if (column instanceof HtmlColumnNew && ((HtmlColumnNew) column).isHideColumn()) {
+                continue;
+            }
+
+            // Render the beginning of this cell
+            boolean isRowHeader = false;
+            Object rowHeaderValue = column.getAttributes().get("rowHeader");
+            if (null != rowHeaderValue) {
+                isRowHeader = Boolean.valueOf(rowHeaderValue.toString());
+            }
+            if (isRowHeader) {
+                writer.startElement("th", column);
+                writer.writeAttribute("scope", "row", null);
+            } else {
+                writer.startElement("td", column);
+                //********************************************** ADD butter style class
+                StringBuilder sb = new StringBuilder("butter-component-table-column");
+                if (column instanceof HtmlColumnNew) {
+                    HtmlColumnNew htmlColumn = (HtmlColumnNew) column;
+                    if (StringUtils.isNotEmpty(htmlColumn.getStyleClass())) {
+                        sb.append(" ").append(htmlColumn.getStyleClass());
+                    }
+                }
+                writer.writeAttribute("class", sb.toString(), null);
+                writer.writeAttribute("columnNumber", "" + columnNumber, null);
+
+                //********************************************** hide column if models says that
+                if (column instanceof HtmlColumnNew && table instanceof HtmlTable && this.isHideColumn((HtmlTableNew) table, (HtmlColumnNew) column)) {
+                    writer.writeAttribute("style", "display:none", null);
+                } else if (column instanceof HtmlColumnNew) {
+                    HtmlColumnNew htmlColumn = (HtmlColumnNew) column;
+                    if (StringUtils.isNotEmpty(htmlColumn.getStyle())) {
+                        writer.writeAttribute("style", htmlColumn.getStyle(), null);
+                    }
+                }
+            }
+
+            // Render the contents of this cell by iterating over
+            // the kids of our kids
+            for (Iterator<UIComponent> gkids = getChildren(column);
+                 gkids.hasNext(); ) {
+                encodeRecursive(context, gkids.next());
+            }
+
+            // Render the ending of this cell
+            if (isRowHeader) {
+                writer.endElement("th");
+            } else {
+                writer.endElement("td");
+            }
+            writer.writeText("\n", table, null);
+
+            columnNumber++;
+        }
+
+    }
+
     private boolean isRowSelected(final HtmlTableNew table, final int rowIndex) {
         if (table.getSingleSelectionListener() != null) {
             final Object rowObject = findRowObject(table, rowIndex);
@@ -254,6 +327,18 @@ public class TableRenderer extends MojarraTableRenderer {
                 // event is not fired by table component
             }
         }
+    }
+
+    private boolean isHideColumn(final HtmlTableNew table, final HtmlColumnNew column) {
+        if (table.getTableColumnVisibilityModel() != null) {
+            final String tableUniqueIdentifier = table.getModelUniqueIdentifier();
+            final String columnUniqueIdentifier = column.getModelUniqueIdentifier();
+            final Boolean hideColumn = table.getTableColumnVisibilityModel().isColumnHidden(tableUniqueIdentifier, columnUniqueIdentifier);
+            if (hideColumn != null) {
+                return hideColumn;
+            }
+        }
+        return column.isHideColumn();
     }
 
     private Object findRowObject(final HtmlTableNew table, final int row) {
