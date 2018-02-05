1ceba61686494a50cc586fa4031130f238d0307f
larmic <mail@larmic.de>
2018-02-05 14:29:07 +0100

    #119 fixed repeat showcase

--- showcase/src/main/java/de/larmic/butterfaces/component/showcase/container/examples/StargateRepeatListCssCodeExample.java
+++ showcase/src/main/java/de/larmic/butterfaces/component/showcase/container/examples/StargateRepeatListCssCodeExample.java
@@ -15,7 +15,7 @@ public class StargateRepeatListCssCodeExample extends CssCodeExample {
     public StargateRepeatListCssCodeExample() {
         addCss(".repeat-episode-list", "padding: 0 10px", "max-height: 400px", "overflow: auto");
         addCss(".repeat-episode-item", "border: #ddd 1px solid", "border-radius: 4px");
-        addCss(".repeat-episode-item.col-md-4", "padding: 5px", "margin: 5px", "width: calc(33.33333333% - 12px)");
+        addCss(".repeat-episode-item.col-md-4", "padding: 5px", "width: calc(33.33333333% - 12px)");
         addCss(".repeat-episode-item img", "width: 100%");
         addCss(".repeat-episode-item repeat-episode-content", "opacity: 0.6", "position: absolute", "top: 0", "color: white", "margin-top: 10px", "margin-left: 10px", "height: 100%", "width: 100%");
         addCss(".repeat-episode-item repeat-episode-content a", "position: inherit", "right: 25px", "bottom: 20px");
