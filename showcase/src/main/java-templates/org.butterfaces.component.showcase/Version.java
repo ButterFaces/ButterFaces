327ceef4738a7749b453d3b2eee4ee541a23b2cd
larmic <mail@larmic.de>
2018-07-09 20:39:46 +0200

    #204 Update Wildflyswarm 2018.5.0 to Thorntail 2.0.0.Final

--- showcase/src/main/java-templates/org.butterfaces.component.showcase/Version.java
+++ showcase/src/main/java-templates/org.butterfaces.component.showcase/Version.java
@@ -14,7 +14,7 @@ public class Version implements Serializable {
     private static final String GROUPID = "${project.groupId}";
     private static final String ARTIFACTID = "${project.artifactId}";
     private static final String REVISION = "${buildNumber}";
-    private static final String JBOSS = "${version.wildfly.swarm}";
+    private static final String JBOSS = "${io.thorntail}";
 
     public String getVersion() {
         return VERSION;
