16ece3c214774191a41089da048d6853726d719c
Lars Michaelis <mail@larmic.de>
2020-01-29 17:22:38 +0100

    #259 add missing surefire plugin

---
+++ components/src/test/java/org/butterfaces/component/html/text/validator/DefaultNumberValidatorTest.java
@@ -0,0 +1,29 @@
+package org.butterfaces.component.html.text.validator;
+
+import org.butterfaces.component.html.text.HtmlNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class DefaultNumberValidatorTest {
+
+    private final DefaultNumberValidator validator = new DefaultNumberValidator();
+
+    @Test
+    void validateWithValueIsNull() {
+        final HtmlNumber htmlNumber = createHtmlNumberMock("0", "10");
+
+        validator.validate(null, htmlNumber, null);
+
+        assertThat(true).isFalse();
+    }
+
+    private HtmlNumber createHtmlNumberMock(final String min, final String max) {
+        final HtmlNumber htmlNumber = mock(HtmlNumber.class);
+        when(htmlNumber.getMin()).thenReturn(min);
+        when(htmlNumber.getMax()).thenReturn(max);
+        return htmlNumber;
+    }
+}
