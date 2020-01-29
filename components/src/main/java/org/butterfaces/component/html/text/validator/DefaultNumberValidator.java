a97ae85b5691c3ac3142af659965e536518d99e0
Lars Michaelis <mail@larmic.de>
2020-01-29 19:12:19 +0100

    #259 implement DefaultNumberValidatorTest.java and fix bug in DefaultNumberValidator.java

--- components/src/main/java/org/butterfaces/component/html/text/validator/DefaultNumberValidator.java
+++ components/src/main/java/org/butterfaces/component/html/text/validator/DefaultNumberValidator.java
@@ -14,16 +14,46 @@ public class DefaultNumberValidator implements Validator {
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         final HtmlNumber htmlNumber = (HtmlNumber) component;
 
+        if (lowerLimitIsSet(htmlNumber)) {
+            validateValueIsBiggerThan(value, Integer.parseInt(htmlNumber.getMin()));
+        }
+
+        if (upperLimitIsSet(htmlNumber)) {
+            validateValueIsLowerThan(value, Integer.parseInt(htmlNumber.getMax()));
+        }
+    }
+
+    private boolean lowerLimitIsSet(HtmlNumber htmlNumber) {
+        return htmlNumber.getMin() != null && !"".equals(htmlNumber.getMin());
+    }
+
+    private boolean upperLimitIsSet(HtmlNumber htmlNumber) {
+        return htmlNumber.getMax() != null && !"".equals(htmlNumber.getMax());
+    }
+
+    private void validateValueIsBiggerThan(final Object value, final int upperLimit) {
+        final int intValue = parseValue(value);
+
+        if (intValue < upperLimit) {
+            throw new ValidatorException(new FacesMessage("Number is to small", String.format("%s is to small", value)));
+        }
+    }
+
+    private void validateValueIsLowerThan(final Object value, final int lowerLimit) {
+        final int intValue = parseValue(value);
+
+        if (intValue > lowerLimit) {
+            throw new ValidatorException(new FacesMessage("Number is to big", String.format("%s is to big", value)));
+        }
+    }
+
+    private int parseValue(Object value) {
+        if (value == null || "".equals(value.toString().trim())) {
+            throw new ValidatorException(new FacesMessage("No number", String.format("%s is no number", value)));
+        }
+
         try {
-            final long longValue = Long.parseLong(value.toString());
-
-            if (htmlNumber.getMin() != null && !"".equals(htmlNumber.getMin()) && longValue < Integer.valueOf(htmlNumber.getMin())) {
-                throw new ValidatorException(new FacesMessage("Number is to small", String.format("%s is to small", value)));
-            }
-
-            if (htmlNumber.getMax() != null && !"".equals(htmlNumber.getMax()) && longValue > Integer.valueOf(htmlNumber.getMax())) {
-                throw new ValidatorException(new FacesMessage("Number is to big", String.format("%s is to big", value)));
-            }
+            return Integer.parseInt(value.toString());
         } catch (NumberFormatException e) {
             throw new ValidatorException(new FacesMessage("No number", String.format("%s is no number", value)));
         }
--- components/src/test/java/org/butterfaces/component/html/text/validator/DefaultNumberValidatorTest.java
+++ components/src/test/java/org/butterfaces/component/html/text/validator/DefaultNumberValidatorTest.java
@@ -1,10 +1,15 @@
 package org.butterfaces.component.html.text.validator;
 
 import org.butterfaces.component.html.text.HtmlNumber;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.faces.validator.ValidatorException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -12,14 +17,82 @@ class DefaultNumberValidatorTest {
 
     private final DefaultNumberValidator validator = new DefaultNumberValidator();
 
-    @Test
-    @Disabled
-    void validateWithValueIsNull() {
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "  "})
+    void validateWithValueIsBlankAndMinAndMaxIsNull(final String value) {
+        final HtmlNumber htmlNumber = createHtmlNumberMock(null, null);
+
+        assertDoesNotThrow(() -> validator.validate(null, htmlNumber, value));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "  "})
+    void validateWithValueIsBlankAndMinAndMaxIsEmpty(final String value) {
+        final HtmlNumber htmlNumber = createHtmlNumberMock("", "");
+
+        assertDoesNotThrow(() -> validator.validate(null, htmlNumber, value));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "  "})
+    void validateWithValueIsBlankAndMinAndMaxIsSet(final String value) {
         final HtmlNumber htmlNumber = createHtmlNumberMock("0", "10");
 
-        validator.validate(null, htmlNumber, null);
+        final ValidatorException exception = assertThrows(ValidatorException.class, () -> validator.validate(null, htmlNumber, value));
 
-        assertThat(true).isFalse();
+        assertThat(exception.getFacesMessage().getSummary()).isEqualTo("No number");
+        assertThat(exception.getFacesMessage().getDetail()).isEqualTo(value + " is no number");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "  "})
+    void validateWithValueIsBlankAndMinIsNotSetAndMaxIsSet(final String value) {
+        final HtmlNumber htmlNumber = createHtmlNumberMock(null, "10");
+
+        final ValidatorException exception = assertThrows(ValidatorException.class, () -> validator.validate(null, htmlNumber, value));
+
+        assertThat(exception.getFacesMessage().getSummary()).isEqualTo("No number");
+        assertThat(exception.getFacesMessage().getDetail()).isEqualTo(value + " is no number");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "  "})
+    void validateWithValueIsBlankAndMinIsSetAndMaxIsNotSet(final String value) {
+        final HtmlNumber htmlNumber = createHtmlNumberMock("0", null);
+
+        final ValidatorException exception = assertThrows(ValidatorException.class, () -> validator.validate(null, htmlNumber, value));
+
+        assertThat(exception.getFacesMessage().getSummary()).isEqualTo("No number");
+        assertThat(exception.getFacesMessage().getDetail()).isEqualTo(value + " is no number");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3"})
+    void validateWithValueInRange(final String value) {
+        final HtmlNumber htmlNumber = createHtmlNumberMock("1", "3");
+
+        assertDoesNotThrow(() -> validator.validate(null, htmlNumber, value));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "0", "4", "5"})
+    void validateWithValueOutOfRange(final String value) {
+        final HtmlNumber htmlNumber = createHtmlNumberMock("1", "3");
+
+        final ValidatorException exception = assertThrows(ValidatorException.class, () -> validator.validate(null, htmlNumber, value));
+
+        if (Integer.parseInt(value) < 1) {
+            assertThat(exception.getFacesMessage().getSummary()).isEqualTo("Number is to small");
+            assertThat(exception.getFacesMessage().getDetail()).isEqualTo(value + " is to small");
+        } else {
+            assertThat(exception.getFacesMessage().getSummary()).isEqualTo("Number is to big");
+            assertThat(exception.getFacesMessage().getDetail()).isEqualTo(value + " is to big");
+        }
     }
 
     private HtmlNumber createHtmlNumberMock(final String min, final String max) {
